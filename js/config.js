8eedd19bfe24a3e13c733692a0c065be83f8c050
tzbd2y4l <99532352+tzbd2y4l@users.noreply.github.com>
2025-08-13 10:46:21 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -38,7 +38,7 @@ const API_SITES = {
     api: 'http://ffzy5.tv/api.php/provide/vod',
     name: '非凡影视',
     api: 'https://json.heimuer.xyz/api.php/provide/vod',
-    name: '黑木耳'
+    name: '黑木耳',
     api: 'https://360zy.com/api.php/provide/vod',
     name: '360资源',
     api: 'https://www.iqiyizyapi.com/api.php/provide/vod',
@@ -56,19 +56,19 @@ const API_SITES = {
     api: 'https://www.mdzyapi.com/api.php/provide/vod',
     name: '魔都资源',
     api: 'https://api.zuidapi.com/api.php/provide/vod',
-    name: '最大资源'
+    name: '最大资源',
     api: 'https://m3u8.apiyhzy.com/api.php/provide/vod',
-    name: '樱花资源'
+    name: '樱花资源',
     api: 'https://api.apibdzy.com/api.php/provide/vod',
-    name: '百度云资源'
+    name: '百度云资源',
     api: 'https://api.wujinapi.me/api.php/provide/vod',
-    name: '无尽资源'
+    name: '无尽资源',
     api: 'https://wwzy.tv/api.php/provide/vod',
-    name: '旺旺短剧'
+    name: '旺旺短剧',
     api: 'https://ikunzyapi.com/api.php/provide/vod',
-    name: 'iKun资源'
+    name: 'iKun资源',
     api: 'https://cj.lziapi.com/api.php/provide/vod/',
-    name: '量子资源站'
+    name: '量子资源站',
 
 
         adult: true,
